49dd2302cbe8a1efae7dc838ae4301aee02fb479
focusotter <mtliendo@focusotter.com>
2023-10-05 23:01:22 -0500

    add next steps before heading to bed

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,16 +1,16 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
 
 export class BackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props)
 
-    // The code that defines your stack goes here
+		//add function
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		// add auth
+
+		// add buckets
+
+		// add envvars to function
+	}
 }
